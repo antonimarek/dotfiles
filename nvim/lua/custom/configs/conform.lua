bd4a8de4e8a055d53326c173f9b27fe650c058b7
amarek <antoni.marek@lumilook.com>
2024-01-23 00:06:32 +0100

    feat: use nvchad

---
+++ nvim/lua/custom/configs/conform.lua
@@ -0,0 +1,20 @@
+local options = {
+	lsp_fallback = true,
+
+	formatters_by_ft = {
+		lua = { "stylua" },
+
+		javascript = { "prettier" },
+    python = { "isort", "black" },
+
+		sh = { "shfmt" },
+	},
+
+	format_on_save = {
+	  -- These options will be passed to conform.format()
+	  timeout_ms = 500,
+	  lsp_fallback = true,
+	},
+}
+
+require("conform").setup(options)
